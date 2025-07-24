b2e269448e50884addb39e08882839ca0e285575
yoonmi12 <yoong12@codeidea.dev>
2025-07-24 17:06:38 +0900

    기본셋팅

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>B.CON</title>
+
+
+    <!-- css -->
+    <link rel="stylesheet" href="./css/font.css" />
+    <link rel="stylesheet" href="./css/reset.css" />
+    <link rel="stylesheet" href="./css/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="./css/style.css" />
+
+    <!-- js -->
+    <script src="./js/jquery-1.12.4.js"></script>
+    <script src="./js/swiper-bundle.min.js"></script>
+    <script src="./js/common.js"></script>
+
+</head>
+<body>
+    <header></header>
+    <main id="wrap"></main>
+    <footer></footer>
+</body>
+</html>
